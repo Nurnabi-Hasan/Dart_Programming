c5b289b36bb2cae902dfdf87f1c8197a7a86a532
Nurnabi-Hasan <nurnabihasan212@gmail.com>
2024-06-24 22:24:20 +0600

    Asynchronus Programming Practice

--- Dart_OOP/practice2.1.dart
+++ Dart_OOP/practice2.1.dart
@@ -1,9 +1,29 @@
-import 'dart:io';
-import 'dart:math';
+Future <num> sum<T extends num>(T a, T b)async{
+  await Future.delayed(Duration(seconds: 4));
+  return a+b;
+}
 
-void main(){
-  print('Please enter a temperature in Celsius:');
 
-  String? input = stdin.readLineSync()!;
+void display(){
+  print('saturday');
+  print('sunday');
+  Future.delayed(Duration(seconds: 5)).then((_){print('monday');});
+  print('tuesday');
+  print('wednesday');
+  print('thrusday');
+}
+
+num summtion<T extends num>(T a, T b){
+  return a+b;
+}
+
+Future <void> main()async{
+
+  num result = await sum(20.50, 20);
+  print(result);
+
+  display();
+ print(summtion(20.5, 20));
 
 }
+
