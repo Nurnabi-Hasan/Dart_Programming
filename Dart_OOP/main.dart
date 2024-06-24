f79d616dd72392490f532b8c8467f4811b9aaadc
Nurnabi-Hasan <nurnabihasan212@gmail.com>
2024-06-24 20:20:23 +0600

    Main mthod for Getter_ Setter

---
+++ Dart_OOP/main.dart
@@ -0,0 +1,29 @@
+import 'Getter_Setter.dart';
+
+void main(){
+
+   For getter Setter Method
+
+  Employee nur = Employee();
+
+  //se value without keyword
+ // nur.setSalary(6000);
+
+  //get Value without keyword
+//  print(nur.getSalary());
+
+//set value using keyword
+  nur.setSalary=6000;
+
+  //get value without keyword
+  print(nur.getSalary);
+
+  print( nur.name);
+  print(nur.designation);
+
+
+
+
+
+
+}
